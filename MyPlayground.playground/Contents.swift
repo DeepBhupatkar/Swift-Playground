efd060a0c59551721d9da5c4512117bf803a14b8
Deep Bhupatkar <bhupatkardeep@gmail.com>
2024-07-18 10:45:51 +0530

    Protocol

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -331,3 +331,75 @@ class  Wall {
 var wall1 = Wall()
 
 // MARK: End of init() Example
+
+// MARK: Start Of Protocol () Example
+
+protocol Greet {
+  
+  // blueprint of property
+  var name: String { get }
+
+  // blueprint of a method
+  func message()
+}
+
+// conform class to Greet protocol
+class Employee: Greet {
+
+  // implementation of property
+  var name = "Perry"
+
+  // implementation of method
+  func message() {
+    print("Good Morning", name)
+  }
+}
+
+var employee1 = Employee()
+employee1.message()
+
+
+// 2nd Example of protocol
+// create Sum protocol
+protocol Sum {
+
+  func addition()
+}
+
+// create Multiplication protocol
+protocol Multiplication {
+
+  func product()
+}
+
+// conform class to two protocols
+class Calculate: Sum, Multiplication {
+
+  var num1 = 0
+  var num2 = 0
+
+  func addition () {
+    let result1 = num1 + num2
+    print("Sum:", result1)
+  }
+
+  func product () {
+    let result2 = num1 * num2
+    print("Product:", result2)
+  }
+                   
+}
+
+// create an object
+var calc1 = Calculate()
+
+// assign values to properties
+calc1.num1 = 5
+calc1.num2 = 10
+
+// access methods
+calc1.addition()
+calc1.product()
+
+
+// MARK: End Of Protocol () Example
