f609f252f144a1fc9b9818c10d54b16fee538579
Deep Bhupatkar <bhupatkardeep@gmail.com>
2024-08-29 11:11:52 +0530

    Updated

--- SwiftPractice .playground/Contents.swift
+++ SwiftPractice .playground/Contents.swift
@@ -48,3 +48,8 @@ let newSUV = SUV.createNewCar()
 
 newSedan.describe() // Output: This is a Black Sedan.
 newSUV.describe()   // Output: This is a Red SUV.
+
+
+let multiplier = 3
+let message = "\(multiplier) times 2.5 is \(Double(multiplier) * 2.5)"
+// message is "3 times 2.5 is 7.5"
